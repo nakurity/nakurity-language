66447b78957e830a68fa405fda142ccd7369521d
Nakurity <nakurity@nakashireyumi.com>
2025-10-30 17:22:06 +0700

    Update and rename parser.py to __main__.py

--- src/base-code/NPP/__main__.py
+++ src/base-code/NPP/__main__.py
@@ -1,15 +1,17 @@
 # this module parses and translate .npp files to the nvm bytecode format
 from pathlib import Path
 import os
+import uuid
 
-class NPP:
+class Bytecode:
   def __init__(self, input):
     self.input = input
     self.filename = input.get("filename", "")
     self.contents = input.get("filecontents", "")
+    self.header = f"uuid: {uuid.v4()}\ntype: npp[fileheader]\ncompiled: false"
 
   def parse(self):
-    bytecode = f":file {self.filename}\n:start-bytecode\n"
+    bytecode = f":file {self.filename}\n[bytecode-header]\n{self.header}\n[end-header]\n:start-bytecode\n"
     acknowledged_modules = []
     for line in self.contents.split("\n"):
       if line.startsWith("@"):
@@ -34,8 +36,37 @@ class NPP:
             break # breaks out of the cur loop
             continue # skips this iteration of the padent loop
           continue # continues searching until array is empty
-        bytecode += f"<namespace:{module[0]} path({path})>\n"
+        bytecode += f"<load:{module[0]} path({path})>\n"
         acknowledged_modules.append(module[0])
       else:
         bytecode += f"<module [{line}]>\n"
     bytecode += ":end-bytecode\n"
+    self.contents = bytecode
+    
+  def compile(self):
+    # expects self.contents to be bytecode
+    load_namespaces = []
+    namespace_passage = []
+    reached_bytecode = False
+    for line in self.contents.split("\n"):
+      if reached_bytecode == True:
+        if line.startsWith("<namespace:"):
+          namespace_passage.append(line)
+        elif line.startsWith("<load:"):
+          load_namespaces.append(
+            line.replace("<load:", "", 1).replace(")>", "", 1).replace("path(", "", 1)
+          )
+        continue
+      elif line.startsWith(":"):
+        line = line.replace(":", "", 1)
+        if line == "start-bytecode":
+          reached_bytecode = True
+          continue
+        continue
+      continue
+    
+    header = self.header.replace("compiled: false", "compiled: true", 1)
+    self.contents = self.contents.replace(f"[bytecode-header]\n{self.header}\n[end-header]", f"[bytecode-header]\n{header}\n[end-header]", 1)
+    self.header = header
+    
+    
