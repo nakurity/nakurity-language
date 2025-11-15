42a1153bfe09f01abbc096f391b0e1bc353958da
Nakurity <nakurity@nakashireyumi.com>
2025-11-15 09:41:05 +0000

    Update some stuff to the ctuils

---
+++ workspace/cutils/temporary-script.sh
@@ -0,0 +1,17 @@
+#                     | =========================================================== |
+# =================== | This file is used to put most recently used commands here,  |
+# | Temporary stuff | | so I don't have to keep typing them. This is mostly because |
+# =================== |          the android keyboard sucks for coding.             |
+#                     | =========================================================== |
+
+# Set it to use bash when possible
+#!/usr/bin/bash
+
+git pull # gotta get the latest changes, since codespaces is just used to run
+
+# ================================================================================= #
+#                  This is where you can write your script                          #
+#         Probably should rename to something like xt, if used too much             #
+# ================================================================================= #
+
+pnpm test
